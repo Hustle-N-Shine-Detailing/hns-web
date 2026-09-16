15e641adeebf19a23def2b77894222594d925974
Joshua Baldwin <Mrbaldwin@hustlenshine.pro>
2026-09-16 13:21:13 -0600

    Load prospect stage persistence fix

--- ops/estimate-tools.js
+++ ops/estimate-tools.js
@@ -98,5 +98,6 @@
   loadScriptOnce('ops-pricing-tools', './pricing-tools.js');
   loadScriptOnce('ops-direct-job-tools', './direct-job-tools.js');
   loadScriptOnce('ops-lead-finder-tools', './lead-finder-tools.js');
+  loadScriptOnce('ops-prospect-tools', './prospect-tools.js');
   loadScriptOnce('ops-photo-upload-tools', './photo-upload-tools.js');
 })();
